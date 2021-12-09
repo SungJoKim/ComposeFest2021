1f9bfa4d0fd9a0b71580f748a36e8c070e2a4f54
sungjoKim <kcd19th@gmail.com>
2021-12-09 23:11:29 +0900

    [week4-2] step 11

--- week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -39,6 +39,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
@@ -127,31 +128,31 @@ private fun PlantDescription(description: String) {
 @Composable
 private fun PlantDetailContentPreview() {
     val plant = Plant("id", "Apple", "HTML<br><br>description", 3, 30, "")
-    MaterialTheme {
+    MdcTheme {
         PlantDetailContent(plant)
     }
 }
 
 @Preview
 @Composable
-private fun PlantDescriptionPreview() {
-    MaterialTheme {
-        PlantDescription("HTML<br><br>description")
+private fun PlantNamePreview() {
+    MdcTheme {
+        PlantName("Apple")
     }
 }
 
 @Preview
 @Composable
 private fun PlantWateringPreview() {
-    MaterialTheme {
+    MdcTheme {
         PlantWatering(7)
     }
 }
 
 @Preview
 @Composable
-private fun PlantNamePreview() {
-    MaterialTheme {
-        PlantName("Apple")
+private fun PlantDescriptionPreview() {
+    MdcTheme {
+        PlantDescription("HTML<br><br>description")
     }
 }
